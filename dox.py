daf942b6f7b34c548a13518374dc86d91ea971d1
Z3RO <99695665+z3ro0fc@users.noreply.github.com>
2022-02-20 19:32:42 -0300

    Create dox.py

---
+++ dox.py
@@ -0,0 +1,92 @@
+from colorama import Fore, init
+init()
+
+#$
+rojo = Fore.RED
+azul = Fore.BLUE
+blanco = Fore.WHITE
+violeta = Fore.MAGENTA
+cyan = Fore.CYAN
+amarillo = Fore.YELLOW
+verde = Fore.GREEN
+#$
+
+print(f"""{violeta}
+
+░░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄▄
+░░░░░█░░░░░░░░░░░░░░░░░░▀▀▄
+░░░░█░░░░░░░░░░░░░░░░░░░░░░█
+░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█
+░▄▀░▄▄▄░░█▀▀▀▀▄▄█░░░██▄▄█░░░░█
+█░░█░▄░▀▄▄▄▀░░░░░░░░█░░░░░░░░░█
+█░░█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄░█
+░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█
+░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█
+░░░█░░░░██░░▀█▄▄▄█▄▄█▄▄██▄░░█
+░░░░█░░░░▀▀▄░█░░░█░█▀█▀█▀██░█
+░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█
+░░░░░░░▀▄▄░░░░░░░░░░░░░░░░░░░█
+░░▐▌░█░░░░▀▀▄▄░░░░░░░░░░░░░░░█
+░░░█▐▌░░░░░░█░▀▄▄▄▄▄░░░░░░░░█
+░░███░░░░░▄▄█░▄▄░██▄▄▄▄▄▄▄▄▀
+░▐████░░▄▀█▀█▄▄▄▄▄█▀▄▀▄
+░░█░░▌░█░░░▀▄░█▀█░▄▀░░░█
+░░█░░▌░█░░█░░█░░░█░░█░░█
+░░█░░▀▀░░██░░█░░░█░░█░░█
+░░░▀▀▄▄▀▀░█░░░▀▄▀▀▀▀█░░█
+░░░░░░░░░░█░░░░▄░░▄██▄▄▀
+░░░░░░░░░░█░░░░▄░░████
+░░░░░░░░░░█▄░░▄▄▄░░▄█
+░░░░░░░░░░░█▀▀░▄░▀▀█
+░░░░░░░░░░░█░░░█░░░█
+░░░░░░░░░░░█░░░▐░░░█
+░░░░░░░░░░░█░░░▐░░░█
+░░░░░░░░░░░█░░░▐░░░█
+░░░░░░░░░░░█░░░▐░░░█
+░░░░░░░░░░░█░░░▐░░░█
+░░░░░░░░░░░█▄▄▄▐▄▄▄█
+░░░░░░░▄▄▄▄▀▄▄▀█▀▄▄▀▄▄▄▄
+░░░░░▄▀▄░▄░▄░░░█░░░▄░▄░▄▀▄
+░░░░░█▄▄▄▄▄▄▄▄▄▀▄▄▄▄▄▄▄▄▄█{violeta}
+{cyan} ＰＲＯＹＥＣＴＯ ： ＴＲＯＬＬＤＯＸ {cyan} 
+{violeta} Created by Z3RO{violeta}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan} Dni = Informacion de un Dni Mundial
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}direccion = Direccion y nombres completos por dni 
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}EpicDox = Doxeo Completo de un numero telefonico
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan} Tool Publica De H-krs:
+""")
+
+dni = input(" ESCRIBE EL DNI : ")
+if not True:
+    print("El dni debe de ser de un menor o incorrecto")
+else:
+        print(f"""{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan} NOMBRE {cyan} {amarillo} = {amarillo} {violeta} Betrorello bautista andres {violeta}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan} DNI {cyan} {amarillo} = {amarillo}{violeta}  47876099
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan} DIRECCION = Güemes 1644 Zarate, Buenos Aires, Argentina{cyan}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Foto Dni = {cyan} {verde}47876099.jpg{verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Huella Izquierda = {cyan}{verde}i47876099.jpg {verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Huella Derecha = {cyan}{verde}d47876099.jpg 
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Madre = {cyan} {verde} Celeste Andrea Rosario{verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Padre = {cyan} {verde} Lautaro German Betrorello{verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Tia = {cyan} {rojo} NO IDENTIFICADO {rojo}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan} Tio = {cyan} {rojo} NO IDENTIFICADO {rojo}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Abuelo = {cyan} {verde} Emanuel Betrorello {verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Abuela = {cyan} {verde} Angela Fernandez {verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan} Facebook = {cyan} {verde} https://m.facebook.com/profile.php?id=100037406384705 {verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Instagram = {cyan}{rojo} NO POSEE {rojo}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Fecha De Nacimiento = {cyan} {verde} 10/1/07 {verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Patente Padre = {cyan}{verde} HKS 092 {verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}DENUNCIAS = {cyan}{rojo} NO TIENE NINGUNA DENUNCIA{rojo}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Numeros De Telefono = {cyan} {verde} +54 9 3487 23-0977 No confirmado{cyan}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Edad = {cyan}{verde} 15 {verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Trabajo = {cyan}{rojo} NO TIENE TRABAJO {rojo}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Estudio Escolar = {cyan}{verde} Escuela N°1 {verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan} Estudio Secundario = {cyan}{verde} Tatoo {verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Estudio Terciario = {cyan} {rojo} NO LO COMENZO {rojo}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Estudio universitario = {cyan} {rojo} NO LO COMENZO {rojo}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan} Usuario Free Fire = {cyan} {verde} Antronixggg {verde}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan} Usuario Play 4 = {cyan} {rojo} NO IDENTIFICADO {rojo}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Usuarios en Redes sociales = {cyan} {verde} LauuuGam { verde }
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}Tarjetas vonculadas = {cyan} {rojo} NO IDENTIFICADAS {rojo}
+{rojo}[{rojo}{amarillo}+{amarillo}{rojo}]{rojo}{cyan}""")
